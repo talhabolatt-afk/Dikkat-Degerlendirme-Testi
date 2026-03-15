45e5bb6373b98590379a0ec82c7db2ea103e875a
talhabolatt-afk <talhabolatt@gmail.com>
2026-03-15 05:24:17 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'dikkat-testi-v2';
+const ASSETS = [
+  '/Dikkat-Degerlendirme-Testi/',
+  '/Dikkat-Degerlendirme-Testi/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
